01d6d5aa5b7688e9b3ef4e7d4fb26d23f1c870f2
Giovanni Fassini Felipe <giovanni20pro@gmail.com>
2024-06-11 17:23:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import './components/paginainicial.css';
+
+// eslint-disable-next-line
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render();
